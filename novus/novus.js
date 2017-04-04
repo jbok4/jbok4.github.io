a74075d1c6622ed5566cb5db37e5ba4be2d53cb7
jbok4 <jbok4@aol.com>
2017-04-04 19:31:37 -0400

    json feeding in properly

--- novus/novus.js
+++ novus/novus.js
@@ -1,16 +1,16 @@
 var data = [
-{  month: 'Jan',  profits: 100000,  inventory: 4000  },
-{  month: 'Feb',  profits: 120000,  inventory: 4500  },
-{  month: 'Mar',  profits: 150000,  inventory: 4700  },
-{  month: 'Apr',  profits: 90000,  inventory: 3200   },
-{  month: 'May',  profits: 75000,  inventory: 3000   },
-{  month: 'Jun',  profits: 87000,  inventory: 2000   },
-{  month: 'Jul',  profits: 190000,  inventory: 500   }
+{  month: "Jan",  profits: 100000,  inventory: 4000  },
+{  month: "Feb",  profits: 120000,  inventory: 4500  },
+{  month: "Mar",  profits: 150000,  inventory: 4700  },
+{  month: "Apr",  profits: 90000,  inventory: 3200   },
+{  month: "May",  profits: 75000,  inventory: 3000   },
+{  month: "Jun",  profits: 87000,  inventory: 2000   },
+{  month: "Jul",  profits: 190000,  inventory: 500   }
 ];
 
 
 
-for (var i = 0; i < data.length; i++) {
+for (var i = 0; i<data.length; i++) {
 document.getElementById("month" + i).innerHTML= data[i].month;
 document.getElementById("element" + i).innerHTML= data[i].profits;
 document.getElementById("bar" + i).style.width = data[i].profits/200 + "px";
